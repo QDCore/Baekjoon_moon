c7095a2a9f50b0468979a102963d0ac0202c624f
QDCore <www.sungrins4@gmail.com>
2023-02-08 19:12:58 +0900

    Baek-moon

---
+++ multiply_A_B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class multiply_A_B {
+    public static void main(String[] args){
+        Scanner ac = new Scanner(System.in);
+
+        int a = ac.nextInt();
+        int b = ac.nextInt();
+
+        System.out.println(a * b);
+    }
+}

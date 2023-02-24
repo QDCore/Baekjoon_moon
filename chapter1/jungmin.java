34f8a5a50914c86afc2887ee30b8ae542e3ffef4
QDCore <www.sungrins4@gmail.com>
2023-02-24 15:55:32 +0900

    jung

---
+++ chapter1/jungmin.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class jungmin {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
